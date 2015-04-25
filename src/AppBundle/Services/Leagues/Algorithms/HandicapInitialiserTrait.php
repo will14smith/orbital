8037b932368f8a950401ca6547f9048ae0e38a71
Will Smith <will@toxon.co.uk>
2015-04-25 21:59:32 +0100

    Created HandicapInitialiserTrait tests

--- src/AppBundle/Services/Leagues/Algorithms/HandicapInitialiserTrait.php
+++ src/AppBundle/Services/Leagues/Algorithms/HandicapInitialiserTrait.php
@@ -13,26 +13,26 @@ trait HandicapInitialiserTrait
      */
     public function init(array $people)
     {
-        usort($people, function(LeaguePerson $a, LeaguePerson $b) {
-            $ah = $a->getPerson()->getCurrentHandicap();
-            $bh = $b->getPerson()->getCurrentHandicap();
+        usort($people, function(LeaguePerson $personA, LeaguePerson $personB) {
+            $handicapA = $personA->getPerson()->getCurrentHandicap();
+            $handicapB = $personB->getPerson()->getCurrentHandicap();
 
-            if(!$ah && !$bh) {
+            if(!$handicapA && !$handicapB) {
                 return 0;
             }
-            if(!$ah) {
-                return 1;
-            }
-            if(!$bh) {
+            if(!$handicapA) {
                 return -1;
             }
+            if(!$handicapB) {
+                return 1;
+            }
 
-            return $ah->getHandicap() - $bh->getHandicap();
+            return $handicapB->getHandicap() - $handicapA->getHandicap();
         });
 
-        $i = 1;
+        $position = 1;
         foreach($people as $person) {
-            $person->setInitialPosition($i++);
+            $person->setInitialPosition($position++);
         }
 
         return $people;
---
+++ src/AppBundle/Tests/Services/Leagues/Algorithms/HandicapInitialiserTraitTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AppBundle\Tests\Services\Leagues\Algorithms;
+
+use AppBundle\Entity\LeaguePerson;
+use AppBundle\Entity\Person;
+use AppBundle\Entity\PersonHandicap;
+use AppBundle\Tests\Services\BaseTestCase;
+
+class HandicapInitialiserTraitTest extends BaseTestCase
+{
+    public function test()
+    {
+        // init - 2 people (no hc, 1 hc, 2 hc, same hc)
+    }
+
+    private function getPerson($hc = null)
+    {
+        $leaguePerson = new LeaguePerson();
+        $person = new Person();
+
+        if($hc !== null) {
+            $handicap = new PersonHandicap();
+            $handicap->setHandicap($hc);
+
+            $person->addHandicap($handicap);
+        }
+
+        $leaguePerson->setPerson($person);
+
+        return $leaguePerson;
+    }
+
+    public function testNoPeople()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $result = $trait->init([]);
+
+        $this->assertCount(0, $result);
+    }
+
+    public function testOnePerson()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $person = $this->getPerson(50);
+        $result = $trait->init([$person]);
+
+        $this->assertCount(1, $result);
+        $this->assertEquals(1, $person->getInitialPosition());
+    }
+    public function testOnePersonNoHandicap()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $person = $this->getPerson();
+        $result = $trait->init([$person]);
+
+        $this->assertCount(1, $result);
+        $this->assertEquals(1, $person->getInitialPosition());
+    }
+
+    public function testTwoPeopleNoHandicaps()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $result = $trait->init([$this->getPerson(), $this->getPerson()]);
+
+        $this->assertCount(2, $result);
+    }
+    public function testTwoPeopleOneHandicap()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $person1 = $this->getPerson(50);
+        $person2 = $this->getPerson();
+        $result = $trait->init([$person1, $person2]);
+
+        $this->assertCount(2, $result);
+        $this->assertEquals(2, $person1->getInitialPosition());
+        $this->assertEquals(1, $person2->getInitialPosition());
+
+        $person1 = $this->getPerson();
+        $person2 = $this->getPerson(50);
+        $result = $trait->init([$person1, $person2]);
+
+        $this->assertCount(2, $result);
+        $this->assertEquals(1, $person1->getInitialPosition());
+        $this->assertEquals(2, $person2->getInitialPosition());
+    }
+    public function testTwoPeopleDifferentHandicaps()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $person1 = $this->getPerson(50);
+        $person2 = $this->getPerson(45);
+        $result = $trait->init([$person1, $person2]);
+
+        $this->assertCount(2, $result);
+        $this->assertEquals(1, $person1->getInitialPosition());
+        $this->assertEquals(2, $person2->getInitialPosition());
+    }
+    public function testTwoPeopleSameHandicaps()
+    {
+        $trait = new HandicapInitialiserTraitStub();
+
+        $person1 = $this->getPerson(50);
+        $person2 = $this->getPerson(50);
+        $result = $trait->init([$person1, $person2]);
+
+        $this->assertCount(2, $result);
+    }
+}
