27e81ac2cffddb4dbcf9cf295ff7de3fa9abd605
Will Smith <will@toxon.co.uk>
2016-02-20 20:25:59 +0000

    Implemented 5-zone metric handicap calculator

--- src/AppBundle/Services/Handicap/TargetCalculatorFactory.php
+++ src/AppBundle/Services/Handicap/TargetCalculatorFactory.php
@@ -5,6 +5,7 @@ namespace AppBundle\Services\Handicap;
 use AppBundle\Entity\RoundTarget;
 use AppBundle\Services\Enum\ScoreZones;
 use AppBundle\Services\Handicap\Calculators\ImperialTargetCalculator;
+use AppBundle\Services\Handicap\Calculators\Metric5ZoneTargetCalculator;
 use AppBundle\Services\Handicap\Calculators\MetricTargetCalculator;
 use AppBundle\Services\Handicap\Calculators\WorcesterTargetCalculator;
 
@@ -22,6 +23,8 @@ class TargetCalculatorFactory
         switch ($roundTarget->getScoringZones()) {
             case ScoreZones::METRIC:
                 return new MetricTargetCalculator($compound);
+            case ScoreZones::TRIPLE:
+                return new Metric5ZoneTargetCalculator($compound);
             case ScoreZones::IMPERIAL:
                 return new ImperialTargetCalculator();
             case ScoreZones::WORCESTER:
