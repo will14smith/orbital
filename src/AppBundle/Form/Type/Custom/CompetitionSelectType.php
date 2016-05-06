911e168ab46961c1a87d85d032b989fe25939c71
Will Smith <will@toxon.co.uk>
2016-05-06 13:50:17 +0100

    Order competition select & add date to label

--- src/AppBundle/Form/Type/Custom/CompetitionSelectType.php
+++ src/AppBundle/Form/Type/Custom/CompetitionSelectType.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Form\Type\Custom;
 
+use AppBundle\Constants;
+use AppBundle\Entity\Competition;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,7 +14,14 @@ class CompetitionSelectType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'class' => 'AppBundle:Competition'
+            'class' => 'AppBundle:Competition',
+            'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('c')
+                    ->orderBy('c.date', 'DESC');
+            },
+            'choice_label' => function (Competition $competition) {
+                return $competition->getName() . ' - ' . $competition->getDate()->format(Constants::DATE_FORMAT);
+            }
         ]);
     }
 
